343aa7781a216d0c91e2d6914ae950dde2d8bdd6
mniedre <webcane@ya.ru>
2014-04-10 23:48:27 +0400

    добавил почуть русского текста

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 /* my custom scripts */
 jQuery(document).ready(function() {
     // todo
+	
+	$('#rate-carousel').carousel({
+		interval:   4000
+	});
+	
+	var clickEvent = false;
+	$('#rate-carousel').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	})
+	.on('slide.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length -1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
 });
