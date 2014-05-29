900cd10df2e359e09b1acccc5acb00d0511b41bf
mniedre <webcane@ya.ru>
2014-05-29 23:08:08 +0400

    hiding header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,49 @@
-﻿/* my custom scripts */
+﻿/**
+ * Author: Heather Corey
+ * jQuery Simple Parallax Plugin
+ *
+ */
+ 
+(function($) {
+ 
+    $.fn.parallax = function(options) {
+ 
+        var windowHeight = $(window).height();
+ 
+        // Establish default settings
+        var settings = $.extend({
+            speed        : 0.15
+        }, options);
+ 
+        // Iterate over each object in collection
+        return this.each( function() {
+ 
+        	// Save a reference to the element
+        	var $this = $(this);
+ 
+        	// Set up Scroll Handler
+        	$(document).scroll(function(){
+ 
+    		        var scrollTop = $(window).scrollTop();
+            	        var offset = $this.offset().top;
+            	        var height = $this.outerHeight();
+ 
+    		// Check if above or below viewport
+			if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+				return;
+			}
+ 
+			var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+ 
+                 // Apply the Y Background Position to Set the Parallax Effect
+    			$this.css('background-position', 'center ' + yBgPosition + 'px');
+                
+        	});
+        });
+    }
+}(jQuery));
+
+/* my custom scripts */
 jQuery(document).ready(function() {
 	
 	/** mandrill callback */
@@ -143,4 +188,10 @@ jQuery(document).ready(function() {
 			}
 			clickEvent = false;
 		});
+	
+	/** Parallax */
+	
+	$('#home').parallax({
+		speed :	0.15
+	});	
 });
