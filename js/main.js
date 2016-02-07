374f98529b90e7972fd894070c124833808f5bdc
Mikhail Niedre <webcane@ya.ru>
2016-02-08 01:22:15 +0300

    add form script

--- js/main.js
+++ js/main.js
@@ -31,8 +31,54 @@ $(document).ready(function() {
       paginationSpeed : 400,
       singleItem: true
   });
+  
+  initMandrillCallback();
 });
 
+function initMandrillCallback() {
+
+
+  	$("#mandrill-callback").click(function() {
+    var name = $("#callback-name").val();
+    var tel = $("#callback-tel").val();
+
+    $.ajax({
+      type : "POST",
+      url : "https://mandrillapp.com/api/1.0/messages/send.json",
+      data : {
+        "key" : "S1ZYU-q2dFyoD04-kXhucw",
+        "message" : {
+          "from_email" : "callback@plasticrestore.github.io",
+          "from_name" : "обратный звонок",
+          "to" : [ {
+            "email" : "webcane@yandex.ru",
+            "type" : "to"
+          } ],
+          "autotext" : "true",
+          "subject" : "callback",
+          "html" : "<h2>Перезвонить</h2>" +
+          "<ul><li><b>Имя</b>: " + name + "</li>" +
+          "<li><b>Телефон</b>: " + tel + "</li>"
+        },
+        "async" : false
+      }
+    })
+    .done(function(response) {
+      alert('Ваш запрос был успешно отправлен!');
+      // reset field after successful submission
+      name.val('');
+      tel.val('');
+    })
+    .fail(function(response) {
+      alert('Во время отправки запроса произошла ошибка.');
+    });
+
+    // чтоб не перебрасывало на /? страницу
+    return false;
+	});
+}
+
+// yandex.map
 ymaps.ready(init);
 
 function init() {
